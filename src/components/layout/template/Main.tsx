41923adc4d7291258d9aa9cdc8918ce0065fe286
Ruan Ferreira <droderuan@hotmail.com>
2023-07-15 18:49:52 -0300

    feat: add roboto as default font family

--- src/components/layout/template/Main.tsx
+++ src/components/layout/template/Main.tsx
@@ -7,7 +7,7 @@ interface MainTemplate {
 
 const MainTemplate: React.FC<MainTemplate> = ({ appName, children }) => {
   return (
-    <body className="dk:bg-sky-950 bg-slate-10 dk:text-gray-50 text-slate-900">
+    <body className="dk:bg-sky-950 font-sans bg-slate-10 dk:text-gray-50 text-slate-900">
       <header className="h-28  md:h-60">
         <div className="m-auto flex h-full max-w-xl items-center p-3 md:max-w-4xl xl:max-w-7xl">
           <h1 className="text-4xl font-medium lg:text-6xl whitespace-pre-wrap">
