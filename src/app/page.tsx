cd5395ed9700b6168ee5bdda0545c3b9599fde33
Ruan Ferreira <droderuan@hotmail.com>
2023-07-15 23:49:10 -0300

    refactor: improvementrs at book listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 import Page from "@/components/layout/structure/Page";
 import Section from "@/components/layout/structure/Section";
-import HorizontalBookList from "@/components/organisms/HorizontalBookList";
-import Image from "next/image";
+import HorizontalBookList from "@/components/organisms/horizontalBookList";
 import Listdata from "../../.local/list_all_books_best_sellers_sample.json";
+import SkeletonHorizontalBookList from "@/components/organisms/skeletonHorizontalBookList";
 
 export default function Home() {
+  const loading = false;
   return (
     <Page>
-      {Listdata.results.lists.map((list) => (
-        <Section key={list.list_id}>
-          <HorizontalBookList name={list.display_name} books={list.books} />
-        </Section>
-      ))}
+      {loading ? (
+        <>
+          <Section>
+            <SkeletonHorizontalBookList />
+          </Section>
+          <Section>
+            <SkeletonHorizontalBookList />
+          </Section>
+          <Section>
+            <SkeletonHorizontalBookList />
+          </Section>
+        </>
+      ) : (
+        Listdata.results.lists.map((list) => (
+          <Section key={list.list_id}>
+            <HorizontalBookList name={list.display_name} books={list.books} />
+          </Section>
+        ))
+      )}
     </Page>
   );
 }
--- src/components/molecules/bookCard/index.tsx
+++ src/components/molecules/bookCard/index.tsx
@@ -1,7 +1,12 @@
 import { BookDto } from "@/client/dtos";
 import Skeleton from "@/components/atoms/skeleton";
+import SpanDisplay from "@/components/atoms/SpanDisplay";
+import SvgIcon from "@/components/atoms/SvgIcon";
 import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
+import AmazonIcon from "../../../../public/amazon.svg";
+import AppleBooksIcon from "../../../../public/apple-ibooks.svg";
 
 export interface BookCardProps {
   book: BookDto;
@@ -9,21 +14,57 @@ export interface BookCardProps {
 
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
-    <div className="w-56 h-96  border-2 relative border-rose-600">
+    <div className="w-60 h-[450px] p-1  relative bg-slate-300 rounded bg-opacity-25 flex flex-col justify-between">
       <div className="absolute top-1 z-30 left-1 bg-slate-900 text-slate-100 bg-opacity-80 bg-trans rounded p-1">
         <span className="inline-block font-bold">#{book.rank}</span>
       </div>
-      <div className="h-60 w-44 m-auto relative z-10 top-0">
-        <Suspense fallback={<Skeleton variant="fill" />}>
-          <Image
-            alt={`${book.title}-book-cover-img`}
-            src={book.book_image}
-            fill
+      <div>
+        <div className="h-44 w-32 m-auto relative z-10 top-0">
+          <Suspense fallback={<Skeleton variant="fill" />}>
+            <Image
+              alt={`${book.title}-book-cover-img`}
+              src={book.book_image}
+              fill
+            />
+          </Suspense>
+        </div>
+        <div>
+          <h3 className="font-medium line-clamp-2 text-ellipsis">
+            {book.title}
+          </h3>
+          <SpanDisplay
+            display="Author:"
+            value={book.author}
+            defaultValue="Missing"
           />
-        </Suspense>
+          <SpanDisplay
+            display="Publisher:"
+            value={book.publisher}
+            defaultValue="Missing"
+          />
+          <SpanDisplay
+            display="Weeks on list:"
+            value={book.weeks_on_list}
+            defaultValue="Missing"
+          />
+        </div>
       </div>
-      <div className="p-1">
-        <h3 className="font-medium line-clamp-3 text-ellipsis">{book.title}</h3>
+      <div>
+        <p className="font-normal text-xs mb-1">Check out!</p>
+        <div className="flex items-center gap-1">
+          <SvgIcon
+            size="normal"
+            src={AmazonIcon}
+            alt="Buy on amazon"
+            link="http:\/\/www.amazon.com\/Ends-Us-Novel-Colleen-Hoover-ebook\/dp\/B0176M3U10?tag=NYTBSREV-20"
+          />
+          <SvgIcon
+            size="sm"
+            src={AppleBooksIcon}
+            alt="Buy on apple books"
+            link="http:\/\/www.amazon.com\/Ends-Us-Novel-Colleen-Hoover-ebook\/dp\/B0176M3U10?tag=NYTBSREV-20"
+          />
+        </div>
       </div>
     </div>
   );
