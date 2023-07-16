bc58159772e446d66b118b49a0fa1dd23114b77b
Ruan Ferreira <droderuan@hotmail.com>
2023-07-15 23:48:36 -0300

    feat: test like icon on the list name

--- src/components/organisms/horizontalBookList/index.tsx
+++ src/components/organisms/horizontalBookList/index.tsx
@@ -1,6 +1,7 @@
 import { BookDto } from "@/client/dtos";
+import Image from "next/image";
 import BookCard, { BookCardProps } from "../../molecules/bookCard";
-
+import LikeSvg from "../../../../public/heart-svgrepo-com.svg";
 export interface HorizontalBookListProps {
   name: string;
   books: BookDto[];
@@ -12,10 +13,18 @@ const HorizontalBookList: React.FC<HorizontalBookListProps> = ({
 }) => {
   return (
     <div>
-      <h2 className="antialiased font-light mb-1 text-xl lg:text-2xl">
-        {name}
+      <h2 className="antialiased font-light mb-1 text-xl lg:text-2xl inline">
+        {name}{" "}
+        <Image
+          priority
+          src={LikeSvg}
+          height={24}
+          width={24}
+          alt="like the list name"
+          className=" inline"
+        />
       </h2>
-      <div className={"overflow-auto"}>
+      <div className="p-1 overflow-auto">
         <div className={"inline-flex gap-1"}>
           {books.map((book) => (
             <BookCard key={book.book_uri} book={book} />
